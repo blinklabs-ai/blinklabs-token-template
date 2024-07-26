ed4058967e54378281caca6b4889f6a7f313edf3
Mike Zhang <mikexd.dev@gmail.com>
2024-07-26 17:18:59 +0800

    update the nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "dep.infinitigenesis.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
