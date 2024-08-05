553a6a17f6a4895ea30c694e91736717050d9680
Mike Zhang <mikexd.dev@gmail.com>
2024-08-05 16:30:58 +0800

    update the hostname

--- next.config.mjs
+++ next.config.mjs
@@ -32,6 +32,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "prod.ai.api.infinitigenesis.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
