6dcc504477e32b46cc4299b53fe34ebf93997ff1
Mike Zhang <mikexd.dev@gmail.com>
2024-08-28 15:04:20 +0800

    feat: Enhance UI styling in TokenDetails component
    Updated the CSS classes for MarkdownText and its child components in TokenDetails.tsx to improve readability and visual appeal. Changes include:
    - Adjusted padding, margins, and font sizes for better spacing and hierarchy.
    - Modified text styles (boldness, italics) for emphasis.
    - Added styling for code blocks with a distinct background color and rounded corners.
    - Removed unnecessary div wrapper.
    
    Also updated favicon.ico file.

--- app/(tabs)/TokenDetails.tsx
+++ app/(tabs)/TokenDetails.tsx
@@ -11,41 +11,44 @@ const TokenDetails = () => {
   return (
     <div className="flex flex-col gap-4 p-4">
       <MarkdownText
-        className="p-2 prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 animate-fade-in-up"
+        className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 animate-fade-in-up break-words p-2"
         remarkPlugins={[remarkGfm, remarkMath]}
         components={{
-          div({ children }) {
-            return <div>{children}</div>;
-          },
           h1({ children }) {
-            return <h1 className="text-[32px] font-bold">{children}</h1>;
+            return <h1 className="mb-4 text-2xl font-extrabold">{children}</h1>;
           },
           h2({ children }) {
-            return <h2 className="text-2xl font-bold">{children}</h2>;
+            return <h2 className="mb-3 text-xl font-bold">{children}</h2>;
           },
           h3({ children }) {
-            return <h3 className="text-lg font-bold">{children}</h3>;
+            return <h3 className="mb-2 text-lg font-medium">{children}</h3>;
           },
           h4({ children }) {
-            return <h4 className="text-base font-bold">{children}</h4>;
-          },
-          h5({ children }) {
-            return <h5 className="text-sm font-bold">{children}</h5>;
-          },
-          h6({ children }) {
-            return <h6 className="text-xs font-bold">{children}</h6>;
+            return <h4 className="mb-2 text-base font-medium">{children}</h4>;
           },
           p({ children }) {
-            return <p className="text-base">{children}</p>;
-          },
-          ol({ children }) {
-            return <ol className="list-decimal pl-4 mb-2">{children}</ol>;
+            return (
+              <p className="mb-2 text-sm text-subtle last:mb-0">{children}</p>
+            );
           },
           ul({ children }) {
-            return <ol className="list-disc pl-4 mb-2">{children}</ol>;
+            return <ul className="mb-2 list-disc pl-4">{children}</ul>;
           },
           li({ children }) {
-            return <li className="mb-1 text-base">{children}</li>;
+            return <li className="mb-1 text-sm text-subtle">{children}</li>;
+          },
+          code({ children }) {
+            return (
+              <code className="rounded-md bg-black px-2 text-white">
+                {children}
+              </code>
+            );
+          },
+          strong({ children }) {
+            return <strong className="font-bold">{children}</strong>;
+          },
+          em({ children }) {
+            return <em className="italic">{children}</em>;
           },
         }}
       >
